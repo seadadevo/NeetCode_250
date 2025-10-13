cc7a576352ba5ede0238b9e6c1fa0203d19cee7d
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-10-13 21:34:49 +0300

    solve the problem with the another solution for enhance the accurucy

--- Arrays/2hasDublicates.js
+++ Arrays/2hasDublicates.js
@@ -4,18 +4,12 @@ class Solution {
      * @return {boolean}
      */
     hasDuplicate(nums) {
-        let arr = []
-        for (let i = 0; i < nums.length; i++) {
-            if(arr.includes(nums[i])){
-                return true;
-            }
-            arr.push(nums[i])   
-        }
-        return false
+        const unique = new Set(nums) // [1 , 2 , 3] 
+        return unique.size !== nums.length
     }
 }
 
 
 const solutio1 = new Solution()
 
-console.log(solutio1.hasDuplicate([1, 2, 4, 3]))
+console.log(solutio1.hasDuplicate([1, 2, 2, 3]))
